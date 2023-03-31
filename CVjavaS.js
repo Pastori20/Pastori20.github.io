7ddf403f5ccba5e6d5c37135e075d745266e7eff
Pastori20 <127365379+Pastori20@users.noreply.github.com>
2023-03-30 23:23:50 -0300

    Add files via upload

---
+++ CVjavaS.js
@@ -0,0 +1,17 @@
+
+document.getElementById("datos__button1").onclick = function() {
+    document.getElementById("losp_1").style.display = "block";
+   	document.getElementById("datos__button1").style.display = "none"; 
+
+}
+document.getElementById("datos__button2").onclick = function() {
+    document.getElementById("losp_2").style.display = "block";
+   	document.getElementById("datos__button2").style.display = "none"; 
+}
+document.getElementById("datos__button3").onclick = function() {
+    document.getElementById("losp_3").style.display = "block";
+   	document.getElementById("datos__button3").style.display = "none"; 
+}
+
+
+
